c01ea5dfc0a0de67ab04f4ff9a7a4a582e3b060a
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-16 16:54:17 -0700

    created port and express

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const fs = require('fs');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let notesData = [];
+
+//ROUTES
+
+//create route that takes user to notes.html
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "notes.html"))
+});
+
+//create route that takes user to index.html
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+//create route that reads the db.json file and return notes as JSON
+
+//POST '/api/notes'
