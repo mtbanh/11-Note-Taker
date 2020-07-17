631ddfd9815f7d21dfe9a8078255e25378be0bd1
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-16 22:06:28 -0700

    created api get routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const db = require('.journal.json')
+c
 
 const app = express();
 
@@ -8,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/static", express.static(path.join(__dirname + 'public')));
 
 let notesData = [];
 
@@ -15,14 +18,24 @@ let notesData = [];
 
 //create route that takes user to notes.html
 app.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 });
+
+app.get("/api/notes", (req, res)=>{
+    try{
+        notesData = JSON.parse(notesData);
+
+    }
+    res.json(db)
+})
 
 //create route that takes user to index.html
 app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "index.html"))
+    res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
 //create route that reads the db.json file and return notes as JSON
-
+app.get("", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public/index.html"))
+});
 //POST '/api/notes'
